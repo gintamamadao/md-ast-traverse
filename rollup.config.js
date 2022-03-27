238826b1f84d309f3bbd7e03294f9f5d4e2fcb73
gintamamadao <gintamaginsan@126.com>
2022-03-27 23:58:59 +0800

    feat: new traverse

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,6 @@ import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
 import pkg from './package.json'
-import json from '@rollup/plugin-json'
-import { preserveShebangs } from 'rollup-plugin-preserve-shebangs'
-import externals from 'rollup-plugin-node-externals'
 
 const extensions = ['.js', '.ts']
 
@@ -31,15 +28,6 @@ export default [
     external: Object.keys(pkg.dependencies),
     output: [{ file: pkg.main, format: 'cjs' }],
     plugins: [
-      externals({
-        builtins: true,
-        deps: true,
-        peerDeps: true,
-        optDeps: true,
-        devDeps: true,
-      }),
-      preserveShebangs(),
-      json(),
       typescript(),
       resolve(),
       commonjs(),
